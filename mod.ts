cef4b2278bf5b884c0a9e6f158797e990379df0f
Juho Vepsäläinen <bebraw@gmail.com>
2022-01-10 13:08:59 +0200

    chore: Disable watcher

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
-import { serve } from 'https://deno.land/x/gustwind@v0.16.2/mod.ts'
+import { serve } from 'https://deno.land/x/gustwind@v0.16.3/mod.ts'
 import projectMeta from "./meta.json" assert { type: "json" };
 
-const server = await serve(projectMeta, Deno.cwd());
+const server = await serve(projectMeta, Deno.cwd(), false);
 
 console.log('Head to localhost:3000');
 
