ca337dd63bc1f72f47b9616c9f24cd241c356b7b
Juho Vepsäläinen <bebraw@gmail.com>
2022-01-11 12:55:35 +0200

    chore: Update to a newer version of gustwind

--- mod.ts
+++ mod.ts
@@ -1,11 +1,13 @@
-import { serve } from 'https://deno.land/x/gustwind@v0.16.3/mod.ts'
-import type { ProjectMeta } from 'https://deno.land/x/gustwind@v0.16.3/types.ts'
+import { serve } from 'https://deno.land/x/gustwind@v0.16.4/mod.ts'
+import { getCache } from 'https://deno.land/x/gustwind@v0.16.4/src/cache.ts'
+import type { ProjectMeta } from 'https://deno.land/x/gustwind@v0.16.4/types.ts'
+import twindSetup from './twindSetup.ts';
 
 // This doesn't work on Deno Deploy just yet
 // import projectMeta from "./meta.json" assert { type: "json" };
 
 const projectMeta = await getJson<ProjectMeta>('./meta.json')
-const server = await serve(projectMeta, Deno.cwd(), false);
+const server = await serve(projectMeta, Deno.cwd(), { ...getCache(), twindSetup });
 
 console.log('Head to localhost:3000');
 
