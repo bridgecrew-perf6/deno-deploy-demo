50cc58fa4397a48a4734fb25f6cb84f347a85c3e
Juho Vepsäläinen <bebraw@gmail.com>
2022-01-11 13:07:33 +0200

    chore: Format

--- mod.ts
+++ mod.ts
@@ -1,15 +1,20 @@
-import { serve } from 'https://deno.land/x/gustwind@v0.17.0/mod.ts'
-import { getCache } from 'https://deno.land/x/gustwind@v0.17.0/src/cache.ts'
-import type { ProjectMeta } from 'https://deno.land/x/gustwind@v0.17.0/types.ts'
-import twindSetup from './twindSetup.ts';
+import { serve } from "https://deno.land/x/gustwind@v0.17.0/mod.ts";
+import { getCache } from "https://deno.land/x/gustwind@v0.17.0/src/cache.ts";
+import type { ProjectMeta } from "https://deno.land/x/gustwind@v0.17.0/types.ts";
+import twindSetup from "./twindSetup.ts";
 
 // This doesn't work on Deno Deploy just yet
 // import projectMeta from "./meta.json" assert { type: "json" };
 
-const projectMeta = await getJson<ProjectMeta>('./meta.json')
-const server = await serve({projectMeta, projectRoot: Deno.cwd(), initialCache: { ...getCache(), twindSetup }, mode: 'production'});
+const projectMeta = await getJson<ProjectMeta>("./meta.json");
+const server = await serve({
+  projectMeta,
+  projectRoot: Deno.cwd(),
+  initialCache: { ...getCache(), twindSetup },
+  mode: "production",
+});
 
-console.log('Head to localhost:3000');
+console.log("Head to localhost:3000");
 
 server();
 
